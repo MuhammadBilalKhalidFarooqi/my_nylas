0b025842cd5d08ba89065798de7a618feb07e824
bilal uncle <bilal.khalid.bsai-2024a@cecosian.edu.pk>
2026-06-07 16:46:22 +0500

    created the clean email list

--- my_nylas/test_flask.py
+++ my_nylas/test_flask.py
@@ -6,30 +6,38 @@ from dotenv import load_dotenv
 try:
     load_dotenv(dotenv_path=r'F:\udemy\mera-llama-index-project\.env')
     backend_app = Flask(__name__)
-    data = 'currently nothing!!'
+    email_data = 'currently nothing!!'
     my_nylas = Client(api_key=os.getenv("nylas_api_key_1"))
     emial_messages = []
     @backend_app.route('/webhook', methods = ['POST', 'GET'])
+
+
     def testing_route():
         if request.method == 'GET':
             return request.args.get('challenge', '')
 #data['data']['object']['thread_id']
         elif request.method == 'POST':
-            global data
+            global email_data
             global clean_text
             global thread_messages
             
-            data = request.get_json()
-            my_thread_id = data.get('data', '').get('object', '').get('thread_id')
+            email_data = request.get_json()
+            my_thread_id = email_data.get('data', '').get('object', '').get('thread_id')
             
-            grant_id = data.get('data', {}).get('object', {}).get('grant_id')
+            grant_id = email_data.get('data', {}).get('object', {}).get('grant_id')
             thread_messages = my_nylas.messages.list(grant_id, query_params={'thread_id' : my_thread_id})
+            folders = email_data['data']['object']['folders']
+
+            junk = ["CATEGORY_PROMOTIONS", "CATEGORY_SOCIAL", "CATEGORY_UPDATES"]
+            primary = ['Inbox', "CATEGORY_PERSONAL"]
             
-            for msg in thread_messages.data:
-                
-            
-                my_clean_email  = my_nylas.messages.clean_messages(grant_id, request_body={'message_id': [msg.id]})
-                emial_messages.append(my_clean_email)
+            for x in folders:
+                if x in primary:
+                    for msg in thread_messages.data:
+                                
+                        my_clean_email  = my_nylas.messages.clean_messages(grant_id, request_body={'message_id': [msg.id]})
+                        emial_messages.append(my_clean_email[0][0].conversation.strip())
+                    break        
             emial_messages.reverse()
             #clean_email = my_nylas.messages.clean_messages(grant_id, request_body= {'message_id':[message_id]})
             #clean_text = clean_email + ('\n', '=' * 35, '\n')
